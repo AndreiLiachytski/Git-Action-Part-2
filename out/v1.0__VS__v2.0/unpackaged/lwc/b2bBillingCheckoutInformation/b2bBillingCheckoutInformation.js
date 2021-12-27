96e8c92397c3848574c57b04f20a4630e4057b62
AndreiLiachytski <andrei.liachytski@gmail.com>
2021-12-27 08:43:43 +0000

    Mon Dec 27 08:43:43 UTC 2021

---
+++ out/v1.0__VS__v2.0/unpackaged/lwc/b2bBillingCheckoutInformation/b2bBillingCheckoutInformation.js
@@ -0,0 +1,12 @@
+import { LightningElement, track } from 'lwc';
+
+const LABELS = {
+    title: '3. Billing information'
+}
+
+export default class B2bBillingCheckoutInformation extends LightningElement {
+
+    // ============================ VARIABLES ===========================================
+
+    @track LABELS = LABELS;
+}
